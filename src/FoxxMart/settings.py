6cf9f886d61a44fec588e703ced53cf958bc18f3
MCN10 <mcn10.foxx@gmail.com>
2021-01-25 18:29:30 +0200

    SQL database settings updated

--- src/FoxxMart/settings.py
+++ src/FoxxMart/settings.py
@@ -90,8 +90,11 @@ WSGI_APPLICATION = 'FoxxMart.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS':{
+            'read_default_file':'/Demo1/auth/mysql.cnf',
+        },
+        'NAME': os.path.join(BASE_DIR, 'db.mysql'),
     }
 }
 
